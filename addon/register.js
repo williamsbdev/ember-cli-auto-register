1254fc21f57140184babb98ca165ee35bc20e3b6
Brandon Williams <williamsbdev@gmail.com>
2015-04-09 21:46:54 -0500

    [FEATURE] better error handling when not exporting Ember.Object

--- addon/register.js
+++ addon/register.js
@@ -8,8 +8,11 @@ var registerWithApplication = function(dirName, application) {
     }).forEach(function(moduleName) {
         var module = require(moduleName, null, null, true);
         var fileName =  moduleName.match(/[^\/]+\/?$/)[0];
-        if (!module || !module["default"]) {
-            console.log(dirName + "/" + fileName + ".js did not have a default export.");
+        if (!module ||
+                !module["default"] ||
+                !(module["default"].prototype instanceof Ember.Object)
+           ) {
+            console.log(dirName + "/" + fileName + ".js did not have an Ember.Object as the default export.");
             throw new Error(moduleName + " must export a default to be registered with application.");
         }
         application.register(dirName + ":" + fileName, module["default"]);
