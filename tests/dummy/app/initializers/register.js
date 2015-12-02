1eab37cc2db7874576ac3dd4a4523de916add840
toran billups <toranb@gmail.com>
2015-12-02 11:15:06 -0600

    [DEPENDENCY] upgrading to ember-cli@1.13.8

--- tests/dummy/app/initializers/register.js
+++ tests/dummy/app/initializers/register.js
@@ -1,17 +1,17 @@
-import Ember from "ember";
 import registerWithContainer from "ember-cli-auto-register/register";
 
-export function initialize(container, application) {
+export function initialize() {
     // this is because of the app inside of the ember-addon
     // this does not need, nor should it be done, in a real app
+    var application = arguments[1] || arguments[0];
     application.name = "dummy";
 
     registerWithContainer("repos", application);
 
     //how you might inject the registered objects in routes/controllers
-    Ember.inject.repos = function(name) {
-        return container.lookup("repos:" + name);
-    };
+    // Ember.inject.repos = function(name) {
+    //     return container.lookup("repos:" + name);
+    // };
 }
 
 export default {
