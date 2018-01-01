d3d08967c01410932568b711effeadf891a98fe6
toran billups <toranb@gmail.com>
2018-01-01 12:10:46 -0600

    DEPENDENCY: upgrade to ember-cli v2.17.2

--- tests/dummy/app/repos/bar.js
+++ tests/dummy/app/repos/bar.js
@@ -1,7 +1,8 @@
-import Ember from "ember";
+import { computed } from '@ember/object';
+import EmberObject from '@ember/object';
 
-var BarRepo = Ember.Object.extend({
-    wat: Ember.computed(function() {
+var BarRepo = EmberObject.extend({
+    wat: computed(function() {
         return "bar";
     })
 });
